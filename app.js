dca9b9d4e550ef9f340ff25f4c42afaa1699077b
MichalKomet <gwozdziej.michal@gmail.com>
2022-01-15 13:14:31 +0100

    Init app

---
+++ app.js
@@ -0,0 +1,157 @@
+// Blackjack
+
+const deck = {
+    dwojka: 2,
+    trojka: 3,
+    czworka: 4,
+    piatka: 5,
+    szostka: 6,
+    siodemka: 7,
+    osemka: 8,
+    dziewiatka: 9,
+    dziesiatka: 10,
+    jopek: 10,
+    dama: 10,
+    krol: 10,
+    as: 11
+}
+let players = []
+let key
+class Game {
+    constructor() {
+        this.sum = 0
+        this.hasAs = 0
+        this.isStand = false
+        this.lose = false
+        this.hand = []
+        this.hit()
+        this.hit()
+    }
+
+    // Dobieranie kart (do udoskonalenia względem metody split)
+    hit() {
+        key = Object.keys(deck)[Math.floor(Math.random()*Object.keys(deck).length)]
+        if(deck[key] === 11){
+            this.hasAs += 1
+        }
+        this.sum += deck[key]
+        this.hand.push(key)
+        // this.sum = this.hand.reduce((sum, currentCard) => {
+        //     return sum + deck[currentCard]
+        //     if(deck[currentCard] === 11){
+        //         this.hasAs += 1
+        //     }
+        // }, 0)
+        // console.log(this.hasAs)
+        if(this.sum > 21 && this.hasAs > 0) {
+            this.sum -= 10
+            this.hasAs -= 1
+        } else if(this.sum > 21 && this.hasAs === 0){
+            this.lose = true
+            this.isStand = true
+            console.log(`${this.name} lose! Your score makes more than 21!`)
+        }
+    }
+    // Rozdzielenie kart w ręcę (do zrobienia)
+    split() {
+        if(this.hand[0]===this.hand[1]){
+            this.secondHand = this.hand.pop()
+            this.hit()
+            this.hit()
+        }
+    }
+    blackjack() {
+        if(deck[this.hand[0]]+deck[this.hand[1]]===21) {
+            console.log(`BlackJack! ${this.name} won!`)
+            this.isStand = true
+        }
+    }
+    stand() {
+        this.isStand = true
+    }
+}
+class Dealer extends Game {
+    constructor(Dealer) {
+        super(Dealer);
+        console.log(`Dealer's first card is: ${this.hand[0]}`)
+
+    }
+    check() {
+        if(players.every(player => player.isStand === true)){
+            console.log(this.hand)
+            this.blackjack()
+            while (this.sum < 16) {
+                this.hit()
+            }
+            console.log(this.hand)
+            console.log(this.sum)
+            if (this.sum > 21) {
+                this.lose = true
+                console.log("Dealer's lose! Players win!")
+            } else {
+                let playersInGame = players.filter(inGame => inGame.lose === false && this.sum <= inGame.sum)
+                // console.log(playersInGame)
+                if (playersInGame.length === 0) {
+                    console.log(`Dealer's win! Dealer's sum: ${this.sum}!`)
+                } else {
+                    console.log(`Dealer's sum: ${this.sum}`)
+                    playersInGame.forEach(el =>
+                        console.log(`${el.name}'s win! Sum: ${el.sum}`)
+                    )
+                }
+            }
+        }
+
+    }
+}
+class Player extends Game {
+    constructor(name) {
+        super(name);
+        this.name = name
+        console.log(`${this.name}'s hand: ${this.hand}. ${this.name}'s score is: ${this.sum}`)
+        this.blackjack()
+    }
+}
+
+function choose(choose) {
+    let inGame = players.filter(player => player.isStand === false)
+    inGame.forEach(el => {
+        switch (choose) {
+            case "hit":
+                el.hit()
+                console.log(`${el.name} hits: ${key}. Total score is: ${el.sum}`)
+                break
+            case "split":
+                el.split()
+                break
+            case "stand":
+                el.stand()
+                break
+            default:
+                el.stand()
+                break
+        }
+    })
+    dealer.check()
+}
+function create(name) {
+    return players.push(new Player(name))
+}
+
+const dealer = new Dealer("Dealer")
+
+create("Michal")
+create("John")
+create("Joanna")
+create("Ola")
+create("Jan")
+console.log("-------------------------------")
+choose("hit")
+choose("stand")
+
+
+
+
+
+
+
